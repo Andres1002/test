70832d6f531770d6170650df248ae97382ad0775
Andres1002 <andresb@iastate.edu>
2022-09-09 08:24:40 -0500

    added image

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-data = pd.read_csv(r'C:\Users\Andres\Documents\GitHub\test\SPY.csv')
+from PIL import Image
+
+data = pd.read_csv(r'SPY.csv')
 df = pd.DataFrame(data)
 st.title("Test Graphs using Plotly")
 
@@ -10,6 +12,10 @@ st.plotly_chart(fig, use_container_width=True)
 
 fig = px.bar(df, x='Date', y='Volume',title="Spy Weekly Volume Shares, Year-To_Date-Graph")
 st.plotly_chart(fig, use_container_width=True)
+
+image = Image.open(r'Open_Access_colours_Venn.PNG')
+
+st.image(image, caption='Image taken from Introducing Volcanica: The first diamond open-access journal for volcanology ')
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
