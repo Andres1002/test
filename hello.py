371c139e0b3df2d569faba85ba2e113df301b1ad
Andres1002 <andresb@iastate.edu>
2022-09-09 08:31:12 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,7 +15,7 @@ st.header("Using px.bar and st.plotly_chart")
 fig = px.bar(df, x='Date', y='Volume',title="Spy Weekly Volume Shares, Year-To_Date-Graph")
 st.plotly_chart(fig, use_container_width=True)
 
-st.header("st.image")
+st.header("Using st.image (citation included)")
 image = Image.open(r'Open_Access_colours_Venn.png')
 
 st.image(image, caption='Image taken from Introducing Volcanica: The first diamond open-access journal for volcanology ')
