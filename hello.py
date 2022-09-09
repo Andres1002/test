524e0c5c9d8dd041da89ea75c2841abddf32e6f0
Andres1002 <andresb@iastate.edu>
2022-09-08 23:08:08 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 import streamlit as st
-
-
+import plotly.express as px
 import pandas as pd
-data = pd.read_csv (r'SPY.csv')
+data = pd.read_csv ('SPY.csv')
 df = pd.DataFrame(data)
 close = df["Close"]
 st.title("SPY Weekly, Year-To-Date Graph")
-chart = st.line_chart(close,height=100)
+chart = st.line_chart(close)
 
+fig = px.line(close,title='Life expectancy in Canada')
+st.plotly_chart(fig, use_container_width=True)
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
 # rerun.
