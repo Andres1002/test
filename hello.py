2b48c8400e674e0ce9a8a80e5016e1c0e18a2f64
Andres1002 <andresb@iastate.edu>
2022-09-08 21:25:38 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 data = pd.read_csv (r'SPY.csv')
 df = pd.DataFrame(data, columns= ['Close'])
-st.title("hello")
+st.title("SPY Weekly, Year-To-Date Graph")
 chart = st.line_chart(df)
 
 # Streamlit widgets automatically run the script from top to bottom. Since
