55773ffb7512667ebe754e9d25b3ae91ed517e70
Andres1002 <andresb@iastate.edu>
2022-09-08 21:56:48 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,9 +3,10 @@ import streamlit as st
 
 import pandas as pd
 data = pd.read_csv (r'SPY.csv')
-df = pd.DataFrame(data, columns= ['Close'])
-st.title("SPY Weekly, Year-To-Date Graph")
-chart = st.line_chart(df)
+df = pd.DataFrame(data)
+close = df["Close"]
+st.title("SPY Weekly, Year-To-Date Graph a")
+chart = st.line_chart(close, height=(close.min(axis=0)))
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
