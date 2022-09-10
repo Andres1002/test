921356e5f7fc5e6666d858356ab9a3cff72e2589
Andres1002 <andresb@iastate.edu>
2022-09-10 17:40:51 -0500

    Update Commander.py

--- Commander.py
+++ Commander.py
@@ -3,9 +3,17 @@ import pandas as pd
 from PIL import Image
 from pathlib import Path
 import plotly.graph_objects as go
-
 # Initial Variables
 x=0;
+
+st.title("Testing Platform for URA")
+with st.sidebar:
+    uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+        st.write(bytes_data)
+
 
 ############ ACQUIRE DATA ###################
 path = r'C:\Users\Andres\Documents\GitHub\test\Stock CSVs'
@@ -22,11 +30,13 @@ cnt=len(dfs)
 ######## PLOT ############
 fig = go.Figure()
 while (x<cnt):
+    
     fig.add_trace(go.Scatter(x=dfs[x]["Date"], y=dfs[x]["Close"],name=str(dfs[x].at[0,"file"])))    
     fig.update_layout(
     title="Stock Prices of Selected Stocks", xaxis_title="Date", yaxis_title="Close Price")
     x=x+1
     ## END while
+
 st.plotly_chart(fig, use_container_width=True)    
 
 
@@ -40,7 +50,8 @@ while (x<cnt):
     ## END while
 st.plotly_chart(fig, use_container_width=True)
 
-################ End plot
+################ End plot ############
+
 st.header("Using st.image (citation included)")
 image = Image.open(r'Images/Open_Access_colours_Venn.png')
 st.image(image, caption='Image taken from: Introducing Volcanica: The first diamond open-access journal for volcanology ')
