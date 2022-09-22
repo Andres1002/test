bb79e41d9bef2cbb23cc36be39701f033c369c46
Andres1002 <andresb@iastate.edu>
2022-09-21 22:19:05 -0500

    Added csvparser

--- Commander.py
+++ Commander.py
@@ -1,26 +1,17 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO 
-import os
-from Plotter import barplotter, lineplotter
+import sys
+from plots import barplotter, lineplotter
+from csvparser import csvparser
 # Initial Variables
-x=0;
-dfs = list()
+sys.path.append(r'C:\Users\Andres\Documents\GitHub\test\Functions')
+
+
 ### Acquire Data ###
 st.title("Testing Platform for URA")
 with st.sidebar:
     files = st.file_uploader("Please choose a CSV file", accept_multiple_files=True)
-    for file in files:
-        bytes_data =file.getvalue()
-        st.write("filename:", file.name)
-        
-        stringio = StringIO(file.getvalue().decode("utf-8"))
-        string_data = stringio.read()
+    dfs,df = csvparser(files)
 
-        df= pd.read_csv(file)
-        df['file'] = os.path.splitext(file.name)[0]
-        dfs.append(df)
-        
 
 cnt=len(dfs)
 ################################# END ACQUIRE DATA ###############################
